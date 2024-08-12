4f84cc0b9b28fed01a1256aeeecd29ae68ad7f0e
Amirabbas Fazelinia <utilyre@gmail.com>
2024-08-12 11:50:41 +0330

    feat: print the total number of tries

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,8 @@ fn main() {
         }
     }
 
+    println!("total = {}", NUM_TRIES);
+    println!();
     println!(
         "{:^10} | {:^10} | {:^10} | {:^10}",
         "num", "count", "expected", "off"
