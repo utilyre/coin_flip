4e8bede3fbe9b1f4caa2193f5fa27ddb2a1a5844
Amirabbas Fazelinia <utilyre@gmail.com>
2024-08-12 11:47:01 +0330

    fix: output table

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,32 @@ fn main() {
 
     let mut rng = rand::thread_rng();
     for _ in 0..NUM_TRIES {
-        let mut idx = 0;
+        let mut num = 0;
         loop {
-            idx += 1;
+            num += 1;
             if rng.gen() {
-                *map.entry(idx).or_default() += 1;
+                *map.entry(num).or_default() += 1;
                 break;
             }
         }
     }
 
-    for (idx, count) in map.iter() {
-        println!("{:0>2} => {}", idx, count);
+    println!(
+        "{:^10} | {:^10} | {:^10} | {:^10}",
+        "num", "count", "expected", "off"
+    );
+    println!("{:-^11}+{:-^12}+{:-^12}+{:-^11}", "", "", "", "");
+
+    let mut probability = 1.0;
+    for (&num, &count) in map.iter() {
+        probability *= 0.5;
+
+        println!(
+            "{:>10} | {:>10} | {:>9.3}% | {:>10}",
+            num,
+            count,
+            100.0 * probability,
+            count as f64 - (NUM_TRIES as f64 * probability).floor()
+        );
     }
 }
